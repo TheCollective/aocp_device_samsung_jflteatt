6208856cad7852c03314ec310896ffcd63c5be56
r <r@TheCollective.(none)>
2013-07-07 12:03:54 -0500

    Intial commit

--- aocp.mk
+++ aocp.mk
@@ -1,16 +1,17 @@
 $(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/aocp/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aocp/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aocp/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
 
-PRODUCT_NAME := cm_jflteatt
+TARGET_BOOTANIMATION_NAME := bootanimation_1080_1920
+PRODUCT_NAME := aocp_jflteatt
 PRODUCT_DEVICE := jflteatt
 
